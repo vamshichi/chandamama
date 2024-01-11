031f53f6d1435bd2f5ce95e93bd3d62908b39b8f
Vamshi ch <115910730+vamshichi@users.noreply.github.com>
2024-01-11 15:17:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Chandamama Computers and communication</h1>
+       <p>Is comming soon</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
